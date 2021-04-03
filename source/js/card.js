5c877fa0b593a61049150d5bb46184356284f4e2
Pozharov Roman <ru.roman.ps@gmail.com>
2021-04-03 16:16:51 +0400

    Заполнил и отрисовал карточки

--- source/js/card.js
+++ source/js/card.js
@@ -5,7 +5,7 @@ const CARD_TEMPLATE = document.querySelector('#tmpl');
 const CardSelectors = {
   text: '.catalog__item-text',
   title: '.catalog__item-title',
-  subtitle: '.catalog__item-subtitle',
+  type: '.catalog__item-type',
   list: '.catalog__list',
   quantity: '.catalog__weight-quantity',
   unit: '.catalog__weight-unit',
@@ -16,10 +16,20 @@ const CardSelectors = {
 
 const fillCard = (data) => {
   const card = CARD_TEMPLATE.content.cloneNode(true);
-  const cardNode = getNodes(card, CardSelectors)
+  const cardNode = getNodes(card, CardSelectors);
+  const {title, type, list, quantity, unit, description, buy, text} = cardNode;
 
+  text.textContent = data.text;
+  title.textContent = data.title;
+  type.textContent = data.type;
+  list.textContent = data.features;
+  console.log(data)
+  quantity.textContent = data.quantity;
+  unit.textContent = data.unit;
+  description.textContent = data.description[0];
+  buy.textContent = data.buy;
+
+  return card;
 }
 
-fillCard()
-
-export {}
+export {fillCard}
--- source/js/utils.js
+++ source/js/utils.js
@@ -1,16 +1,12 @@
 const getNodes = (parent, selectors) => {
-  let nodes = {};
   const keys = Object.entries(selectors);
-  console.log(keys);
-  console.log(selectors);
-  for (let [key, value] of keys) {
-    console.log('key: ', key)
-    console.log('value: ', value)
-    nodes[key] = parent.querySelector(value);
-    console.log(nodes[key])
-  }
-  console.log(nodes);
+  let nodes = {};
 
+  for (let [key, value] of keys) {
+    nodes[key] = parent.querySelector(value);
+  }
+
+  return nodes;
 };
 
 export {getNodes}
