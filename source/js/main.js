9249761bbacddad14bdd236b965403b61bf49aa0
Pozharov Roman <ru.roman.ps@gmail.com>
2021-04-05 15:01:08 +0400

    Добавил data атрибут для изменения состояний

--- source/js/main.js
+++ source/js/main.js
@@ -3,6 +3,9 @@ import {loadData} from './api.js';
 import {fillCard} from './card.js';
 
 const CATALOG = document.querySelector(".content__catalog");
+const TEXT_ITEM_DEFAULT = 'Сказочное заморское яство';
+const TEXT_ITEM_HOVER = 'Котэ не одобряет';
+const STATE_MAX_COUNT = 3;
 
 const renderCard = () => {
   for (let i = 0; i < saveData.length; i++) {
@@ -20,69 +23,39 @@ const handlePageLoadedSuccess = () => {
   .then(handleDataLoadSuccess)
 };
 
-const TEXT_ITEM_DEFAULT = 'Сказочное заморское яство';
-const TEXT_ITEM_HOVER = 'Котэ не одобряет';
-
-// const StateNumber = {
-//   DEFAULT: 0,
-//   SELECT: 1,
-//   DISABLED: 2,
-// };
-
-const STATE_MAX_COUNT = 3;
-
-// соотношение состояний с классами на элементе
 const StateNumber = {
   1: 'catalog__item',
   2: 'catalog__item catalog__item--select',
   3: 'catalog__item catalog__item--disabled',
 };
 
-const CardState = {
-  [StateNumber.DEFAULT]: 'catalog__item',
-  [StateNumber.SELECT]: 'catalog__item catalog__item--select',
-  [StateNumber.DISABLED]: 'catalog__item catalog__item--disabled',
-};
-
-let state = 1;
-
-// function cardClickHandler(evt) {
-//   let child = evt.target;
-//   let parent = evt.currentTarget;
-//   let thisParent = child.closest(".catalog__item");
-//   let attrParent = thisParent.dataset.animal;
-//   let outText = thisParent.querySelector(".catalog__descript");
-//   if (child != parent && !child.classList.contains("catalog__descript")) {
-//     if (thisParent.classList.contains("catalog__item--select")) {
-//       thisParent.classList.toggle("catalog__item--select");
-//       thisParent.classList.add("catalog__item--disabled");
-//       outText.innerHTML = content[attrParent]["disabled"];
-//     } else
-//     if (thisParent.classList.contains("catalog__item--disabled")) {
-//       thisParent.classList.toggle("catalog__item--disabled");
-//       outText.innerHTML = content[attrParent]["default"];
-//     } else {
-//       thisParent.classList.toggle("catalog__item--select");
-//       outText.innerHTML = content[attrParent]["selected"];
-//     }
-//   }
-// };
-
 const cardClickHandler = (evt) => {
   const item = evt.target.closest('.catalog__item');
- 
-  state++;
-  if (state > STATE_MAX_COUNT) {
-    state = 1;
+
+  item.dataset.state++;
+
+  if (item.dataset.state > STATE_MAX_COUNT) {
+    item.dataset.state = 1;
   };
-  item.className = StateNumber[state]; // className использовать - это норм?
+  item.className = StateNumber[item.dataset.state];
 };
 
-const cardMouseOutHandler = (evt) => {
-  console.log(evt.target)
-  if (state === 1) {
+// const cardClickHandler = (evt) => {
+//   const item = evt.target.closest('.catalog__item');
+//   let state = item.getAttribute('data-state');
 
-  }
+//   state++;
+//   item.dataset.state = state;
+//   if (state > STATE_MAX_COUNT) {
+//     state = 1;
+//     item.dataset.state = state;
+//   };
+//   item.className = StateNumber[item.dataset.state];
+// };
+
+const cardMouseOutHandler = (evt) => {
+  // console.log(evt.target)
+
 }
 
 function mouseOutItem(evt) {
